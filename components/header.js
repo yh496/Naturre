a1998b80f7b8d234fe3d8a7828c607e0361dd31e
Jae Won Jung <caroljung619@gmail.com>
2020-09-19 19:47:17 +0900

    add search box

--- components/header.js
+++ components/header.js
@@ -1,11 +1,80 @@
-import styles from '../styles/Header.module.css'
+// import styles from '../styles/Header.module.css'
+import { makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+
+import { Grid } from '@material-ui/core'
+const useStyles = makeStyles((theme) => ({
+  header: {
+    display: 'inline'
+  },
+  title: {
+    color: 'blue',
+    paddingLeft: '30px',
+    paddingTop: '20px',
+    fontSize: '40px',
+    fontWeight: 'bold',
+  },
+  login: {
+    color: 'blue',
+    // paddingTop: '15px',
+    fontSize: '30px',
+    margin: 'auto',
+    transform: 'translate(30%, 0%)'
+
+  },
+  // about: {
+  //   float: 'right',
+  //   color: 'lightblue',
+  //   paddingTop: '15px',
+  //   paddingRight: '10px',
+  //   fontSize: '30px',
+  // },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  root: {
+    padding: '2px 4px',
+    marginTop: '20px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  searchBox: {
+    transform: 'translate(0%,-10%)'
+  }
+}));
 export default function Header() {
-  // const classes = useStyles();
+  const classes = useStyles();
   return (
-    <div>
-      <div className={styles.title}>Naturre</div>
-      <div className={styles.login}>Login/Register</div>
-      <div className={styles.about}>About</div>
+    <div className={classes.header}>
+      <Grid container>
+
+        <Grid item xs={4} className={classes.title}>Naturre</Grid>
+        <Grid item xs={5} className={classes.searchBox}>
+          <Paper component="form" className={classes.root}>
+            <InputBase
+              className={classes.input}
+              placeholder="Search Google Maps"
+              inputProps={{ 'aria-label': 'search google maps' }}
+            />
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Grid>
+        <Grid item xs={3} className={classes.login}>Login/Register</Grid>
+
+
+      </Grid>
+
+      {/* <div className={classes.about}>About</div> */}
 
     </div>
   )
--- pages/business-profile.js
+++ pages/business-profile.js
@@ -2,16 +2,21 @@ import styles from '../styles/Home.module.css'
 
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  content: {
-    fontSize: '40px'
-  },
- 
-});
+// const useStyles = makeStyles({
+//   title: {
+//     color: 'blue',
+//     paddingLeft: '30px',
+//     paddingTop: '10px',
+//     fontSize: '40px',
+//     fontWeight: 'bold',
+//     float: 'left',
+//   },
+
+// });
 
 export default function BusinessProfile() {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
-      <div className={classes.content}><h1>content</h1></div>
+    <div><h1></h1></div>
   )
 }
