c5f40e7cdac51edf0fd45b958aab9036172b5eff
Youn Sung Huh <yh496@cornell.edu>
2020-09-19 19:09:59 +0900

    material-ui nextjs setup completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,63 @@
-import '../styles/globals.css'
-import Header from "../components/header"
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme';
+import Header from '../components/header'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
-    </div>
-  )
+
+export default function MyApp(props) {
+    const { Component, pageProps, serverCookie } = props;
+
+    //let cookie = serverCookie === '' ? cookies : serverCookie;
+
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Naturre</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                   {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                    <CssBaseline />
+                    <Header/> 
+                      <Component {...pageProps} />
+            </ThemeProvider>
+        </React.Fragment>
+    );
 }
 
-export default MyApp
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.object.isRequired,
+};
+
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+    let serverCookie = (ctx.req && ctx.req.headers && ctx.req.headers.cookie) ? parseCookie(ctx.req.headers.cookie) : '';
+
+    if(ctx.res){
+        console.log('server !');
+    }else{
+        console.log('client !');
+    }
+
+    let pageProps = {} // This is how pages will get their own getinitialprops
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx)
+    }
+    if (!pageProps.namespacesRequired) {
+        pageProps.namespacesRequired = ['common']
+    }
+    return { pageProps, serverCookie : serverCookie}
+
+}
--- pages/business-profile.js
+++ pages/business-profile.js
@@ -1,7 +1,17 @@
 import styles from '../styles/Home.module.css'
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  content: {
+    fontSize: '40px'
+  },
+ 
+});
+
 export default function BusinessProfile() {
+  const classes = useStyles();
   return (
-    <div className={styles.container}><h1>content</h1></div>
+      <div className={classes.content}><h1>content</h1></div>
   )
 }
