a54c16a0d2bfe6bee8ba79a29c2b022f03519440
Jae Won Jung <caroljung619@gmail.com>
2020-10-30 23:23:15 -0700

    login,register files

---
+++ pages/login.js
@@ -0,0 +1,6 @@
+
+export default function Login() {
+  return (
+    <div><h1>You can start working here!</h1></div>
+  )
+}
---
+++ pages/register.js
@@ -0,0 +1,6 @@
+
+export default function Register() {
+  return (
+    <div><h1>You can start working here!</h1></div>
+  )
+}
