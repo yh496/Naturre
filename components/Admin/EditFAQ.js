ff9a400241edb919b1f6b41297afd4a13e3ed099
Christy Song <cs966@cornell.edu>
2021-03-24 18:55:20 -0400

    Add bizId field instead of search bar, try to load exising faq with load button, try to handle submit

--- components/Admin/EditFAQ.js
+++ components/Admin/EditFAQ.js
@@ -41,7 +41,6 @@ const useStyles = makeStyles( (theme) => ({
 
 
 export default function AddFAQ (props) {
-
   const classes = useStyles()
 
   // const businessId = "5fccd7e8f8905189825b0ce1";
@@ -50,21 +49,31 @@ export default function AddFAQ (props) {
   const [searchText, setSearchText] = useState("")
   const router = useRouter();
   const businessId = router.query.id
+  const bizId = "5fccd7e8f8905189825b0ce1"
+
+  // const [bizId, setBizId] = useState('')
+  const handleBizIdChange = (val) => {
+    // setBizId(val)
+  }
 
   const [comments, setComments] = useState([])
-  const callApi = (businessId) => {
+  const callApi = (bizId) => {
     fetch(`/api/business-profile/business-questions`, {
       method: 'POST',
       headers: { "Content-Type": "application/json; charset=utf-8" },
-      body: JSON.stringify({ id: businessId })
+      body: JSON.stringify({ id: bizId })
 
     }).then(e => e.json()).then(e =>
       setComments(e.data)
     )
   }
 
+  useEffect(() => {
+    callApi(bizId)
+  }, '')
+
   const loadComments = () => {
-    callApi("5fccd7e8f8905189825b0ce1")
+    callApi(bizId)
   }
 
   const [faqList, setFAQList] = useState([{"question":"", "answer":"", "show": true}])
@@ -78,14 +87,47 @@ export default function AddFAQ (props) {
     console.log(tempFAQList)
   }
   const removeItem = (index) => {
-    const tempFAQList = faqList
-    tempFAQList.splice(index, 1)
-    setFAQList(tempFAQList)
-    console.log(tempFAQList)
+    // const tempFAQList = faqList
+    // tempFAQList.splice(index, 1)
+    // setFAQList(tempFAQList)
+    // console.log(tempFAQList)
+
+// diff version
+    // setFAQList(faqList => {
+    //   const tempFAQList = faqList.filter((item, j) => index !== j);
+    //
+    //   return {
+    //     tempFAQList,
+    //   };
+    // });
   }
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    // const faqs = []
+    // await faqList.forEach((faq) => {
+    //   const question = faq.question;
+    //   const answer = faq.answer;
 
+    const data = {
+      businessId: bizId, faqs: faqList
+    };
 
+    await fetch('http://localhost:3000/api/business-profile/business-questions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(data => {
+        consol.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      });
+  }
   return (
     <div>
       <Card>
@@ -99,10 +141,22 @@ export default function AddFAQ (props) {
           />
           <form>
             <div style={{marginTop: 10, textAlign: 'right'}}>
+              <div style={{display: 'block', marginBottom: '15px', marginTop: '15px', textAlign: 'left'}}>
+                <label for="businessId" style={{fontSize: '13pt', marginRight: '10px', display: 'inline-block', paddingTop: '8px'}}>Business ID:</label>
+                <TextField id="businessId" onChange={e => handleBizIdChange(e.target.value)} style={{width: '60%', marginRight: '10px'}} size="small" label="Enter business ID" variant="outlined" />
+                <Button onClick={() => loadComments()} style={{ height: 30, backgroundColor: '#40bfb0', paddingTop: '5px' }} >load</Button>
+              </div>
+              {comments.map((val, i) => (
+                <Paper>
+                  <div>
+                    <Typography variant="h4" style={{ fontWeight: 700, fontSize: '15px' }}> {val.title} </Typography>
+                    <Typography variant="p"> {val.content} </Typography>
+                  </div>
+                </Paper>
+              ))}
               <Button onClick={() => addNewFAQ()} style={{fontSize: "18pt", height: 30, backgroundColor: '#40bfb0', paddingTop: 2 }} >+</Button>
               {faqList.map((val, i) => (
                 <div className={classes.newFAQ}>
-                
                   <TextField className={classes.newQuestionField} size='small' onChange={e => handleFAQListChange(i, "question", e.target.value)}  id="question1" label="Question" variant="outlined" multiline rowsMax={2} />
                   <TextField onChange={e => handleFAQListChange(i, "answer", e.target.value)} id="answer1" label="Answer" variant="outlined" multiline rowsMax={6}/>
                   <Button variant="contained" startIcon={<DeleteIcon />} className={classes.deleteButton} onClick={() => removeItem(i)}>Delete</Button>
